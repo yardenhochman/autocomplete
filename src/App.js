8ca5847e566b5e259cf98c87734118093cc82f6b
Yarden Hochman <yardenhochman@macbook-pro-2.home>
2020-02-23 01:01:23 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
+import axios from 'axios';
+// import { debounce } from 'lodash';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const fetchResults = (searchText) =>
+  axios.get(`http://localhost:5000/search?q=${searchText}`);
+
+// const includesText = (searchText) => (entry) => entry.term.includes(searchText);
 
 function App() {
+  const [searchText, setSearchText] = React.useState('');
+  const [results, setResults] = React.useState([]);
+
+  const fetchSuggestions = async () => {
+    if (searchText.length > 2) {
+      const res = await fetchResults(searchText);
+      console.log(res);
+      // setResults(fetchSuggestions(value).);
+    } else {
+      setResults([]);
+    }
+  };
+
+  const onChange = async ({ target: { value } }) => {
+    setSearchText(value);
+    fetchSuggestions();
+  };
+
+  const showSuggestions = () => null;
+
+  const selectSuggestion = (term) => {
+    setSearchText(term);
+    setResults([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Page>
+      <SearchBar
+        onChange={onChange}
+        value={searchText}
+        onKeyUp={showSuggestions}
+      />
+      {!!results.length && (
+        <SuggestionList>
+          {results.map(({ resultCount, term }) => (
+            <Suggestion key={term} onClick={() => selectSuggestion(term)}>
+              <span>{`${term} (${resultCount})`}</span>
+            </Suggestion>
+          ))}
+        </SuggestionList>
+      )}
+    </Page>
   );
 }
 
 export default App;
+
+const SearchBar = styled.input.attrs({
+  autocomplete: 'off',
+  type: 'text',
+  name: 'search',
+})`
+  width: 200px;
+  height: 20px;
+  border: gray 1px solid;
+  &:focus {
+    border: black 1px solid;
+  }
+`;
+const Page = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 25px;
+  font-size: 14px;
+  box-sizing: border-box;
+`;
+
+const Suggestion = styled.li.attrs({
+  tabIndex: 0,
+})`
+  padding: 10px 2px;
+  width: 202px;
+  /* padding: 0.75rem 1.15rem; */
+  list-style-type: none;
+  text-align: left;
+  border: solid 0.5px gray;
+
+  & span {
+    padding: 0 10px;
+  }
+`;
+
+const SuggestionList = styled.ul.attrs(() => ({
+  role: 'listbox',
+}))`
+  margin: 0;
+  padding: 0;
+`;
